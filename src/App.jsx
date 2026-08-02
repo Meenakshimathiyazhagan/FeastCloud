145895daf1e49e0995f39829445299f01d196c85
Meenakshimathiyazhagan <145017758+Meenakshimathiyazhagan@users.noreply.github.com>
2026-08-02 20:23:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Services from "./Components/Services";
+import Foodstories from "./Components/Foodstories";
+import Aboutus from "./Components/Aboutus";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Foodstories />
+      <Aboutus />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
